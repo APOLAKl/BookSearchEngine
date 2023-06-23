5d9ca89bd2bd9e3a1863cee8c30bb0a858c7c226
APOLAKl <122183428+APOLAKl@users.noreply.github.com>
2023-06-22 23:24:53 -0300

    Created mutations.js updated loginForm and some server side changes

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { CREATE_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  const [createUser] = useMutation(CREATE_USER)
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+mutation createUser($username: String!, $email: String!, $password: String!) {
+  createUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      email
+      password
+      username
+      saveBooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+}
+`
